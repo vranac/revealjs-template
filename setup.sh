3792d696479405e06b73b88b57116a67f08018d4
Vranac Srdjan <vranac@code4hire.com>
2014-09-20 14:48:59 +0200

    Be explicit so the OS does now bitch.

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ else
 fi
 
 cp -f ./revealjs/index.html ./index.html
-sed -i 's/"css\//"revealjs\/css\//g' index.html
-sed -i 's/"lib\/css/"revealjs\/lib\/css\//g' index.html
-sed -i "s/'css\/print/'revaljs\/css\/print/g" index.html
-sed -i 's/"lib\/js/"revealjs\/lib\/js/g' index.html
-sed -i 's/"js/"revealjs\/js/g' index.html
-sed -i "s/'plugin\//'revealjs\/plugin\//g" index.html
+sed -i='' 's/"css\//"revealjs\/css\//g' index.html
+sed -i='' 's/"lib\/css/"revealjs\/lib\/css\//g' index.html
+sed -i='' "s/'css\/print/'revaljs\/css\/print/g" index.html
+sed -i='' 's/"lib\/js/"revealjs\/lib\/js/g' index.html
+sed -i='' 's/"js/"revealjs\/js/g' index.html
+sed -i='' "s/'plugin\//'revealjs\/plugin\//g" index.html
